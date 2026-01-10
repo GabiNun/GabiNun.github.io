2385fb64036f1e18bcedd0b104486096a2fa6033
Gabi <182965942+GabiNun@users.noreply.github.com>
2026-01-10 09:24:38 +0200

    Update UninstallEdge.cs

--- Source/UninstallEdge.cs
+++ Source/UninstallEdge.cs
@@ -1,6 +1,4 @@
-using System;
 using System.IO;
-using System.Diagnostics;
 
 class Program
 {
@@ -9,12 +7,12 @@ class Program
         string directoryPath = @"C:\Windows\SystemApps\Microsoft.MicrosoftEdge_8wekyb3d8bbwe\";
         System.IO.Directory.CreateDirectory(directoryPath);
 
-        string filePath = System.IO.Path.Combine(directoryPath, "MicrosoftEdge.exe");
-        System.IO.File.Create(filePath).Close();
+        string filePath = Path.Combine(directoryPath, "MicrosoftEdge.exe");
+        File.Create(filePath).Close();
          
         var setup = System.Linq.Enumerable.First(Directory.EnumerateFiles(@"C:\Program Files (x86)\Microsoft\Edge\Application", "setup.exe", SearchOption.AllDirectories));
 
-        Process.Start(setup, "--uninstall --system-level --force-uninstall --delete-profile");    
+        System.Diagnostics.Process.Start(setup, "--uninstall --system-level --force-uninstall --delete-profile");    
     }
 
 }
